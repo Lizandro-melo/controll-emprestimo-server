23047d70cb51551658c6a8c186eee3c28a17242e
Lizandro Melo <Lizandro.melllo@gmail.com>
2025-11-28 00:22:48 -0300

    Fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import { ProviderAlert } from "@/presentation/provider/provider_alert";
+import { ProviderLoading } from "@/presentation/provider/provider_loading";
 import "@/presentation/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ProviderAlert>
+      <ProviderLoading>
+        <Component {...pageProps} />
+      </ProviderLoading>
+    </ProviderAlert>
+  );
 }
