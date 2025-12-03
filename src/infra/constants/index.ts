5e78c305f12d11a600cac8ec6a9c78c628b70bf9
Lizandro <lizandro.melllo@gmail.com>
2025-12-03 02:38:22 -0300

    mudar Api Asass

--- src/infra/constants/index.ts
+++ src/infra/constants/index.ts
@@ -3,4 +3,4 @@ import Assas from "../assas";
 export const DAYS_EXPIRE_SESSION = 30;
 export const DAYS_EXPIRE_SUB = -2;
 export const SALT_ROUNDS = 10;
-export const ASSAS = new Assas(process.env.PAY!, "Sandbox");
+export const ASSAS = new Assas(process.env.PAY!, "Production");
