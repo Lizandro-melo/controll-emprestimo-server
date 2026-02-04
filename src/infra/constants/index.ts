de0614d624fe33d8b4acd4d99c9ccbd3d794ce9a
Lizandro Melo <Lizandro.melllo@gmail.com>
2026-02-04 01:23:17 -0300

    Preparando o ambiente de homologação

--- src/infra/constants/index.ts
+++ src/infra/constants/index.ts
@@ -3,4 +3,4 @@ import Assas from "../assas";
 export const DAYS_EXPIRE_SESSION = 30;
 export const DAYS_EXPIRE_SUB = 1;
 export const SALT_ROUNDS = 10;
-export const ASSAS = new Assas(process.env.PAY!, "Production");
+export const ASSAS = new Assas(process.env.PAY!, "Sandbox");
