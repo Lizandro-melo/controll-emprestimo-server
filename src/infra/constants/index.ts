f547d46cfdc1235cc9ab7d72f9aadac56823ff47
Lizandro Melo <Lizandro.melllo@gmail.com>
2026-02-04 22:08:11 -0300

    Update constant

--- src/infra/constants/index.ts
+++ src/infra/constants/index.ts
@@ -3,4 +3,7 @@ import Assas from "../assas";
 export const DAYS_EXPIRE_SESSION = 30;
 export const DAYS_EXPIRE_SUB = 1;
 export const SALT_ROUNDS = 10;
-export const ASSAS = new Assas(process.env.PAY!, "Sandbox");
+const TYPE_ASAAS: "Sandbox" | "Production" = process.env.TYPE_ASAAS as
+  | "Sandbox"
+  | "Production";
+export const ASSAS = new Assas(process.env.PAY!, TYPE_ASAAS);
